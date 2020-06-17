40407e7a81058ad537670d7dfb10f14091f979d6
parada20 <sandraparada36@gmail.com>
2020-06-17 07:53:15 +0000

    from z to a

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - main function
+ * Return: 0
+ **/
+int main(void)
+{
+	char s;
+
+	for (s = 'z'; s <= 'a'; s++)
+		putchar(s);
+	putchar('\n');
+	return (0);
+}
