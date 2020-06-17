46f4615ef44d86ba053cd594e99c92aa8bf4ceeb
parada20 <sandraparada36@gmail.com>
2020-06-17 08:12:45 +0000

    single_B10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main function
+ * Return: 0
+ **/
+int main(void)
+{
+	int s;
+
+	for (s = 0; s < 10; s++)
+	printf("%d", s);
+	printf("\n");
+	return (0);
+}
