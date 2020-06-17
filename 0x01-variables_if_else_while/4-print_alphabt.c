a9c169a44bb50ae40a1f6959d2bb658c0840804a
parada20 <sandraparada36@gmail.com>
2020-06-17 07:36:00 +0000

    print except some characters

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - main function
+ * Return: 0
+ **/
+int main(void)
+{
+	char s;
+
+	for (s = 'a'; s <= 'z'; s++)
+		putchar(s);
+	if (s != '113' && s != '101')
+		putchar(s);
+	putchar('\n');
+	return (0);
+}
