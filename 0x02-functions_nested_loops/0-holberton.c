cc7e25217966e6b5cdc0240c2c9fbc108241bcce
parada20 <sandraparada36@gmail.com>
2020-06-18 13:53:50 +0000

    Holberton

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,18 +1,19 @@
-#include <holbeton.h>
+ #include "holbeton.h"
 /**
  * Main function
  * Return 0
  */
 int main(void)
 {
-	_putchar("H");
-	_putchar("o");
-	_putchar("l");
-	_putchar("b");
-	_putchar("e");
-	_putchar("r");
-	_putchar("t");
-	_putchar("o");
-	_putchar("n");
-	return 0;
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+	return (0);
 }
