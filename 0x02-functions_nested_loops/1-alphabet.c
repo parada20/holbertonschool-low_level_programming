eb48b57ca672de396486810f3c08957a48e97b0c
parada20 <sandraparada36@gmail.com>
2020-06-18 15:01:33 +0000

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,1 +1,17 @@
-sandra
+#include "holberton.h"
+
+/**
+ * print_alphabet - print alphabet function
+ */
+
+void print_alphabet(void)
+{
+	char x = 'a';
+
+	while (x <= 'z')
+	{
+		_putchar(x);
+		++x;
+	}
+	_putchar('\n');
+}
