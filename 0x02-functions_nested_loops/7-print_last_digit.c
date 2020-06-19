5965d7b0986efcc294ef9aa322044753c2606d3c
parada20 <sandraparada36@gmail.com>
2020-06-19 04:51:02 +0000

    last

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,18 +1,25 @@
 #include "holberton.h"
 
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
+ * print_last_digit - print last digit
+ * @n: Integer
+ * Return: return last digit
  */
-int main(void)
-{
-	int r;
 
-	print_last_digit(98);
-	print_last_digit(0);
-	r = print_last_digit(-1024);
-	_putchar('0' + r);
-	_putchar('\n');
-	return (0);
+int print_last_digit(int n)
+{
+
+	int l;
+
+	l = n % 10;
+
+	if (l < 0)
+	{
+		l = -l;
+	}
+
+
+	_putchar ('0' + l);
+
+	return (l);
 }
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,61 +1,18 @@
 #include "holberton.h"
 
 /**
- * table - print table
+ * times_table - print table
  * @t: integer
  */
-void table(int t)
+void times_table(int t)
 {
-	int m, r;
-
-	for (m = 0; m <= 9; m++)
+	int l;
+	int c;
+	int r;
+	1=0;
+	while (1 < 10) /* 1 <= 9 */
 	{
-		r = t * m;
-		if (m != 0)
-		{
-			if (r > 9)
-			{
-				_putchar(' ');
-			}
-			else
-			{
-				_putchar(' ');
-				_putchar(' ');
-			}
-		}
-
-		if (r > 9)
-		{
-			_putchar ('0' + r / 10);
-			_putchar ('0' + r % 10);
-		}
-		else
-		{
-			_putchar ('0' + r);
-		}
-
-		if (m != 9)
-		{
-			_putchar(',');
-		}
-
+		c = 0;
+		while (c < 10)printf("%d", r);
 	}
-
-}
-
-/**
- * times_table - print tables to 0 at 9
- */
-
-void times_table(void)
-{
-	int t;
-
-	for (t = 0; t <= 9; t++)
-	{
-		table(t);
-		_putchar ('\n');
-	}
-
-
 }
