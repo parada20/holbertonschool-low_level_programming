baf4c359132f01ba134fe0c54dc0349b4c6213d9
parada20 <sandraparada36@gmail.com>
2020-06-19 02:34:55 +0000

    24h

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * jack_bauer - print time from 00:00 to 23:59
+ */
+
+void jack_bauer(void)
+{
+	int h, m;
+
+	for (h = 0; h < 24; h++)
+	{
+
+		for (m = 0; m < 60; m++)
+		{
+			_putchar ('0' + h / 10);
+			_putchar ('0' + h % 10);
+			_putchar (':');
+			_putchar ('0' + m / 10);
+			_putchar ('0' + m % 10);
+			_putchar ('\n');
+		}
+	}
+
+
+}
